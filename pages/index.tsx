ecb3786127df44a6b8084d4f4812cef1594c0546
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2022-01-02 15:57:48 +0100

    infer getStaticProps on main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,18 @@
-export default function Home() {
-  const message: string = "Init site";
-  return <div>{message}</div>;
+import type { InferGetStaticPropsType } from "next";
+
+export async function getStaticProps() {
+  const products = ["product1", "product2", "product3"];
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{products}</div>;
 }
