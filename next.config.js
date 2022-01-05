6dac43d8ebfba9a58ad8562dacca42a1a0ac6542
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2022-01-05 07:53:03 +0100

    continue configuration of tsConfig and config to handle several backends

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const { withFrameworkConfig } = require("./framework/common/config");
 
 module.exports = withFrameworkConfig({
+  framework: {
+    name: "shopify",
+  },
   reactStrictMode: true,
   i18n: {
     locales: ["en-US", "sv"],
     defaultLocale: "en-US",
   },
 });
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2));
